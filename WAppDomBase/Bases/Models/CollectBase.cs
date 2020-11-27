572d47e3e3793d42f3e13903f974800c66bce034
Javier Mendoza Paitan <javier.mendoza.p@upch.pe>
2020-11-27 15:36:30 -0500

    init

---
+++ WAppDomBase/Bases/Models/CollectBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WAppDomBase.Interfaces.Models;
+
+namespace WAppDomBase.Bases.Models
+{
+    public abstract class CollectBase : ICollect
+    {
+        public abstract string Id { get; }
+        public abstract long? SysTrace { get; }
+        public abstract DateTime CreatedOn { get; set; }
+        public abstract DateTime UpdatedOn { get; set; }
+    }
+}
---
+++ WAppDomBase/Interfaces/Models/ICollect.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAppDomBase.Interfaces.Models
+{
+    public interface ICollect
+    {
+        string Id { get; }
+        long? SysTrace { get; }
+        DateTime CreatedOn { get; set; }
+        DateTime UpdatedOn { get; set; }
+    }
+}
